3c892808bd8fc674a1ea705a0e0f4a58c33b6609
gseung02 <jiseung1102@naver.com>
2025-01-03 15:40:28 +0900

    Summery

---
+++ 에어비앤비/trab.js
@@ -0,0 +1,12 @@
+const prev = ()=>{
+  
+}
+
+const init = () => {
+  const prevBtn = document.querySelector('.l-btn');
+  prevBtn.addEventListener('click',()=>{});
+  const nextBtn = document.querySelector('.r-btn');
+  nextBtn.addEventListener('click',()=>{});
+}
+
+window.onload = init;
